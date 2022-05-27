f10b57ce6de7297f7d61fc3f56adda15e9e8f580
Tharissa <tharissaetec@gmail.com>
2022-05-27 08:44:07 -0300

    refactor: Removendo itens desnecessarios

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculadora
+{
+    public class Calculadora
+    {
+        public double Somar(double v1, double v2)
+        {
+            return v1 + v2;
+        }
+
+        public int Dividir(int v1, int v2)
+        {
+            return v1 / v2;
+        }
+    }
+}
